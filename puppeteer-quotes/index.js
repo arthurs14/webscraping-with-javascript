75e25a6008781317968c093d765f59efa815525e
arthurs14 <arthurcs426@gmail.com>
2023-07-10 17:03:55 -0700

    scrapign first page of quotes working

---
+++ puppeteer-quotes/index.js
@@ -0,0 +1,52 @@
+import puppeteer from 'puppeteer';
+
+const getAllQuotes = async () => {
+  // start a puppeteer session with:
+  // - a visible browser ('headless: false' - easier to debug because you'll see the browser in action)
+  // - no default viewport ('defaultViewport: null' - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  // open a new page
+  const page = await browser.newPage();
+
+  // On this new page:
+  // - open the "http://quotes.toscrape.com/" website
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto('http://quotes.toscrape.com/', {
+    waitUntil: 'domcontentloaded',
+  });
+
+  // get page data
+  const quotes = await page.evaluate(() => {
+    // fetch the first element with class "quote"
+    const quoteList = document.querySelectorAll('.quote');
+
+    console.log('quote list ::', quoteList);
+
+    // convert the quotelist to an iterable array
+    // for each quote ftetch the text and author
+    return Array.from(quoteList).map((quote) => {
+      // fetch the sub elements from the previously fetched quote element
+      // get the displayed text and return it ('.innerText')
+      const text = quote.querySelector('.text').innerText;
+      const author = quote.querySelector('.author').innerText;
+
+      return { text, author };
+    });
+  });
+
+  // display the quotes
+  console.log(quotes);
+
+  // click on the "next page" button
+  await page.click('.pager > .next > a');
+
+  // close the browse
+  await browser.close();
+};
+
+// start the scraping
+getAllQuotes();
