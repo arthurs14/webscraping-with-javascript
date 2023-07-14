2cd2535d3445fc7a65c57ed30d75bd089998d5b2
arthurs14 <arthurcs426@gmail.com>
2023-07-13 21:37:53 -0700

    replicated puppeteer get all quotes on cheerio

--- cheerio-quotes/getAllPages.js
+++ cheerio-quotes/getAllPages.js
@@ -1,2 +1,29 @@
 import axios from 'axios';
 import { load } from 'cheerio';
+
+const quotes = [];
+
+const getTwoPageQuotes = async () => {
+  for (let i = 1; i <= 10; i++) {
+    const url = `http://quotes.toscrape.com/page/${i}`;
+    const response = await axios.get(url);
+    const $ = load(response.data);
+
+    // for each quote
+    $('.quote').each((index, el) => {
+      const quote = {
+        id: quotes.length + 1,
+        quote: $(el).find('.text').text(),
+        author: $(el).find('.author').text(),
+        authorUrl: $(el).find('span a').attr('href'),
+      };
+
+      quotes.push(quote);
+    });
+  }
+
+  console.log('length ::', quotes.length);
+  console.log('quotes ::', quotes);
+};
+
+getTwoPageQuotes();
